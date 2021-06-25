ba6366ec795bd96a47eea7d401b11d3eab0b0267
Qiuling Xu <simpleword2014@gmail.com>
2021-06-25 19:02:19 -0400

    Daily sync.

--- algo/classic.py
+++ algo/classic.py
@@ -10,5 +10,5 @@ def knowledge_distill_loss(model, prev_model, x):
     output = model(x, full=True)
     prev_output = prev_model(x)
     output = prev_model.process_output(output)
-    return l2_loss(output - prev_output) * beta
+    return l2_loss(output, prev_output) * beta
 
