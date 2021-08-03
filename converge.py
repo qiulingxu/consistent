Add imagnet dataset, add lwf sep loss baseline. Fix a bug in historical average model.

converge.py
@@ -16,15 +16,16 @@ class ConvergeImprovement():
         self.decay_rate = get_config("convergence_decay_rate")
         self.small_better = use_loss
 
-    def save_model(self, model):
+    def save_model(self, model, step):
         self.best_state = copy.deepcopy(model.state_dict())
+        self.save_step = step
 
     def __call__(self, score, step, model):
         if self.max_score is None:
             self.max_score = score
             self.min_score = score
             self.avg_growth = 1
-            self.save_model(model)
+            self.save_model(model, step)
         else:
             if self.small_better:
                 improve_ratio = (self.min_score - score) / self.max_score 
@@ -33,7 +34,7 @@ class ConvergeImprovement():
             self.avg_growth = self.avg_growth*self.decay_rate + improve_ratio * (1-self.decay_rate)
             if score > self.max_score:
                 self.max_score = score
-                self.save_model(model)
+                self.save_model(model, step)
             if score < self.min_score:
                 self.min_score = score
         if debug and DEBUG:
@@ -60,13 +61,13 @@ class NoImprovement():
             self.min_score = score
             self.step = 0
             self.best_model = model
-            self.save_model(model)
+            self.save_model(model, step)
         else:
             if (not self.small_better and score > self.max_score) or\
                 (self.small_better and score<self.min_score):
                 self.max_score = score
                 self.min_score = score
-                self.save_model(model)
+                self.save_model(model, step)
                 self.step = 0
             else:
                 self.step += 1
@@ -76,8 +77,9 @@ class NoImprovement():
             return False
         else:
             return True
-    def save_model(self, model):
+    def save_model(self, model, step):
         self.best_state = copy.deepcopy(model.state_dict())
+        self.save_step = step
 
     def restore_best_model(self, model:nn.Module):
         model.load_state_dict(self.best_state)

eval.py
@@ -36,6 +36,7 @@ class EvalProgressPerSample(EvalBase):
         self.max_step = max_step
         self.curr_step = 0
         self.device = device
+        self.addtional_data = {}
         self.names: List[Any] = [] # type List[Any] 
         self.orders:Dict[Any, Tuple[str, Any]] =  {} # type 
 
@@ -63,6 +64,9 @@ class EvalProgressPerSample(EvalBase):
             self.orders[name] = ("from",0)
         else:
             self.orders[name] = order
+
+    def save_addition(self, key, data):
+        self.addtional_data[key] = data
 
     def find_match_model(self, models, key):
         ks = models.keys()
@@ -164,10 +168,14 @@ class EvalProgressPerSample(EvalBase):
             for compare_2 in range(compare_1+1, l_steps):
                 step_2 = valid_step[compare_2]
                 cnt = 0
+                tot_cnt = 0
                 for l in range(length):
+                    if INCON_TOT_TYPE == "Correct" and hist[l,step_1]!= 1.0:
+                        break
+                    tot_cnt += 1
                     if hist[l,step_1]>hist[l, step_2] + EPS:
                         cnt += 1
-                full_score.append({"compare": (step_1, step_2), "consistency": cnt * 1.0 / length})
+                full_score.append({"compare": (step_1, step_2), "consistency": tot_cnt * 1.0 / length})
         return {"inconsist_pairwise":full_score, "cnt":length, "index":valid_step}
 
     def _measure(self, names):
@@ -201,7 +209,7 @@ class EvalProgressPerSample(EvalBase):
         _js_file = filename + "_measure.json"
         hist_file = filename + "_hist.npy"
         _measure = self.measure()
-        _config = {"orders":self.orders, "len": self.len, "curr_step":self.curr_step, "names": self.names}
+        _config = {"orders":self.orders, "len": self.len, "curr_step":self.curr_step, "names": self.names,"add":self.addtional_data}
         _measure.update(_config)
         with open(_js_file, "w") as f:
             f.write(json.dumps(_measure))
@@ -216,6 +224,10 @@ class EvalProgressPerSample(EvalBase):
         self.len = _measure["len"]
         self.curr_step = _measure["curr_step"]
         self.names = _measure["names"]
+        if "add" in _measure:
+            self.addtional_data = _measure["add"]
+        else:
+            self.addtional_data = {}
         self.hist_version = np.load(hist_file + ".npz")
     
     def set_order(self, option, val=None):

task.py
@@ -106,6 +106,7 @@ class VanillaTrain(Task):
 
         self.prev_models = {} # type: Dict[str, Dict[Any, nn.Module]]
         tot_step = 0
+        self.converge_step = []
         self.tasks = task2model.keys()
         self.pre_train()
         for tn in self.tasks:
@@ -177,6 +178,8 @@ class VanillaTrain(Task):
                         assert False, "not Implemented"
                 for tn in self.tasks:
                     self.converge[tn].restore_best_model(self.curr_model[tn])
+                self.converge_step.append(self.converge[ctn].save_step)
+                self.evaluator.save_addition("train_step", self.converge_step)
             elif self.granularity == "epoch":
                 if self.multi_task_flag == False:
                     self.curr_task_name = list(task2model.keys())[0]
@@ -188,8 +191,7 @@ class VanillaTrain(Task):
                 assert False, "Implement other time slice definition"
             for task_name, model in self.curr_model.items():
                 self.curr_model[task_name] = self.model_process(task_name, model, order, -1)
-            self.evaluator.eval(self.process_model4eval(self.curr_model))    
-            log("Measure",self.evaluator.measure())
+
             if self.iscopy:
                 self.last_model = self.copy(self.curr_model)
             else:
@@ -202,6 +204,8 @@ class VanillaTrain(Task):
                 else:
                     self.task_var[tn] = var_lst
                 self.prev_models[tn][order] = self.last_model[tn]
+            self.evaluator.eval(self.process_model4eval(self.last_model))    
+            log("Measure",self.evaluator.measure())                
             self.post_task()
             """self.eval(model=model,
                         dataset=self.curr_test_data_loader, \
@@ -216,6 +220,9 @@ class VanillaTrain(Task):
     #    return True
     def post_task(self):
         pass
+
+    def get_converge_step(self):
+        return self.converge_step
 
     def pre_train(self):
         pass